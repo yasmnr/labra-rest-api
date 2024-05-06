ce3b9ae7a595e6f47cd086cdb6e957650b04bc93
Yasmine Rantala <ycbran@utu.fi>
2024-05-07 01:18:56 +0300

    tutoriaali Playing with API

--- api/views.py
+++ api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from rest_framework.response import Response
+
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Uijuma")

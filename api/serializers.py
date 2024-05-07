c9f7f8241bc8d36a8a8f4b62b817db4a90692036
Yasmine Rantala <ycbran@utu.fi>
2024-05-08 01:49:53 +0300

    added names to relationship view

--- api/serializers.py
+++ api/serializers.py
@@ -6,10 +6,26 @@ from api.models import Person, Relationship
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Person
-        fields = ["url", "first_name","last_name", "birth_date","death_date", "gender"]
+        fields = [
+            "url",
+            "first_name",
+            "last_name",
+            "birth_date",
+            "death_date",
+            "gender",
+        ]
 
 
 class RelationshipSerializer(serializers.HyperlinkedModelSerializer):
+    parent_name = serializers.SerializerMethodField()
+    child_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Relationship
-        fields = ["url", "parent", "child", "type"]
+        fields = ["url", "parent", "child", "type", "parent_name", "child_name"]
+
+    def get_parent_name(self, obj):
+        return str(obj.parent)
+
+    def get_child_name(self, obj):
+        return str(obj.child)

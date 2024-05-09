dce4af05f2e06fae0dd099b58f76c96d2d68a2d3
Yasmine Rantala <ycbran@utu.fi>
2024-05-09 23:52:20 +0300

    migrations done

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -5,43 +5,83 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Person',
+            name="Person",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('first_name', models.CharField(max_length=200)),
-                ('last_name', models.CharField(blank=True, max_length=200)),
-                ('birth_date', models.DateField()),
-                ('death_date', models.DateField(blank=True, null=True)),
-                ('gender', models.CharField(choices=[('M', 'Male'), ('F', 'Female')], max_length=1)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("first_name", models.CharField(max_length=200)),
+                ("last_name", models.CharField(blank=True, max_length=200)),
+                ("birth_date", models.DateField()),
+                ("death_date", models.DateField(blank=True, null=True)),
+                (
+                    "gender",
+                    models.CharField(
+                        choices=[("M", "Male"), ("F", "Female")], max_length=1
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Relationship',
+            name="Relationship",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('type', models.CharField(choices=[('father', 'Father'), ('mother', 'Mother')], max_length=20)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "type",
+                    models.CharField(
+                        choices=[("father", "Father"), ("mother", "Mother")],
+                        max_length=20,
+                    ),
+                ),
             ],
         ),
         migrations.AddConstraint(
-            model_name='person',
-            constraint=models.CheckConstraint(check=models.Q(('death_date__isnull', True), ('death_date__gt', models.F('birth_date')), _connector='OR'), name='death_date_is_after_birth_date'),
+            model_name="person",
+            constraint=models.CheckConstraint(
+                check=models.Q(
+                    ("death_date__isnull", True),
+                    ("death_date__gt", models.F("birth_date")),
+                    _connector="OR",
+                ),
+                name="death_date_is_after_birth_date",
+            ),
         ),
         migrations.AddField(
-            model_name='relationship',
-            name='child',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='parents', to='api.person'),
+            model_name="relationship",
+            name="child",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="parents",
+                to="api.person",
+            ),
         ),
         migrations.AddField(
-            model_name='relationship',
-            name='parent',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='children', to='api.person'),
+            model_name="relationship",
+            name="parent",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="children",
+                to="api.person",
+            ),
         ),
     ]
